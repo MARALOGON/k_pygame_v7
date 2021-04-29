13e0c0d4f14a50f32e30de4757daba8b3f2b2bf2
Marcos Alonso <marcosalonso@MacBook-Pro-de-Marcos.local>
2021-04-29 22:11:40 +0200

    Fin de bolas sueltas, a por la raqueta

--- ball.py
+++ ball.py
@@ -1,18 +1,6 @@
 import pygame as pg
 import sys
-from random import randint
-
-def rebotaX(x):
-    if x <=0 or x >=ANCHO:
-        return -1
-
-    return 1
-
-def rebotaY(y):
-    if y <=0 or y >=ALTO:
-        return -1
-
-    return 1
+from random import randint, choice
 
 
 ROJO = (255, 0, 0)
@@ -27,44 +15,57 @@ pantalla = pg.display.set_mode((ANCHO, ALTO))
 reloj = pg.time.Clock()
 
 class Bola():
-    def __init__(self, x, y, vx, vy, color):
+    def __init__(self, x, y, vx=5, vy=5, color=(255, 255, 255), radio=10):
         self.x = x
         self.y = y
         self.vx = vx
         self.vy = vy
         self.color = color
+        self.radio = randint(5, 30)
+
+    def actualizar(self):
+        self.x += self.vx
+        self.y += self.vy
+
+        if self.y <=0 or self.y >=ALTO:
+            self.vy = -self.vy
+    
+        if self.x <=0 or self.x >=ANCHO:
+            self.vx = -self.vx
+        
+    def dibujar(self, lienzo):
+        pg.draw.circle(lienzo, self.color, (self.x, self.y), self.radio)
 
 bolas = []
 
 for _ in range(10):
     bola = Bola(randint(0, ANCHO),
                 randint(0, ALTO),
-                randint(5, 10),
-                randint(5, 10),
+                randint(5, 10)*choice([-1, 1]),
+                randint(5, 10)*choice([-1, 1]),
                 (randint(0, 255), randint(0,255), randint(0,255)))
     
     bolas.append(bola)
 
 game_over = False
 while not game_over:
-    v = reloj.tick(60)
+    v = reloj.tick(10)
+
     #Gestion de eventos
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = True
 
+
     # Modificación de estado
     for bola in bolas:
-        bola.x += bola.vx
-        bola.y += bola.vy
+        bola.actualizar()
 
-        bola.vy *= rebotaY(bola.y)
-        bola.vx *= rebotaX(bola.x)
-
-
+    
     # Gestión de la pantalla
     pantalla.fill(NEGRO)
     for bola in bolas:
+        bola.dibujar(pantalla)
         pg.draw.circle(pantalla, bola.color, (bola.x, bola.y), 10)
 
 
