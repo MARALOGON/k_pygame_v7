6f10d127e58f6e1db29365f203cf4c5b90058aad
Marcos Alonso <marcosalonso@MacBook-Pro-de-Marcos.local>
2021-05-01 08:13:02 +0200

    Primera Version con una  clase

--- ballProvisonal.py
+++ ballProvisonal.py
@@ -10,7 +10,7 @@ def rebotaX(x): #Estas funciones sustituye a los if de las lineas 57 y 61, estab
 
 
 def rebotaY(y):
-    if y <= 0 or y >= ANCHO: 
+    if y <= 0 or y >= ALTO: 
         return -1
     
     return 1
@@ -28,31 +28,34 @@ pg.init()
 pantalla = pg.display.set_mode((ANCHO,ALTO))
 reloj = pg.time.Clock()
 
+class Bola():
+    def __init__(self, x, y, vx, vy, color):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.color = color
+        
+
 bolas = []
 for _ in range(10):
+    bola = Bola(randint(0, ANCHO), #Sustuyo el diciionario bola por la instancia bola, que llama a la clase Bola
+                randint(0, ALTO), 
+                randint(5, 10), 
+                randint(5, 10),
+                (randint(0, 255), randint(0, 255), randint(0, 255)))
+
+    '''
     bola = {'x': randint(0, ANCHO), 
             'y': randint(0, ALTO), 
             'vx': randint(5, 10), 
             'vy': randint(5, 10),
             'color': (randint(0, 255), randint(0, 255), randint(0, 255))
-    
     }
+    '''
+
     bolas.append(bola)
 
-'''
-#Bola 1
-x = ANCHO // 2 # Se pone doble barra para que el resultado de la division sean solo numeros enteros, que nod e decimales
-y = ALTO // 2
-vx = -5 #Esta variable marca la velocidad de la bola
-vy = -5
-
-#Bola 2
-x2 = randint(0, ANCHO)
-y2 = randint(0, ALTO)
-vx2 = randint(5, 15)
-vy2 = randint(5, 15)
-
-'''
 
 game_over = False
 while not game_over:
@@ -63,42 +66,29 @@ while not game_over:
             game_over = True
     
     #Modificacion de estado
-    '''
-    x += vx #Estas coordenadas cambian cada vez que se recorre el bucle for, para que la bola esté cada vez en una posición nueva
-    y += vy
-    x2 += vx2
-    y2 += vy2
-    '''
     for bola in bolas:
-        bola['x'] += bola['vx']
-        bola['y'] += bola['vy']
+        
+        bola.x += bola.vx
+        bola.y += bola.vy
 
-    #vy *= rebotaY(y) #Esta modificacion de estado de la variable vy va en relación de la funcion RebotaY  
-        bola['vy'] *= rebotaY(bola['y'])
-    #vx *= rebotaX(x)
-        bola['vx'] *= rebotaX(bola['x'])
+        bola.vy *= rebotaY(bola.y)
+        bola.vx *= rebotaX(bola.x)
+        
+        '''
+        bola['x'] += bola.vx
+        bola.y += bola.vy
 
-    #vy2 *= rebotaY(y2)
-    #vx2 *= rebotaX(x2)
+        bola.vy *= rebotaY(bola.y)
+        bola.vx *= rebotaX(bola['x'])
+       
+        '''
 
-    '''
-    if y <= 0 or y >= ALTO: #Si la y es <= 0 rebota, es menor que la coordenada vertical superior, o es >= ALTO, es decir, es  mayor que la coordenada vertical inferior
-        vy = -vy #Se iguala al negativo de la variable para que cuando rebote lo haga a la misma velocidad a la que iba, que se ha determinado fuera del while
-    
-
-    if x <= 0 or x >= ANCHO: #Si la x es <= 0 rebota, es menor que la coordenada horizontal izquierda, o x>= ANCHO, es decir, es mator que la coordenada horizontal derecha
-        vx = -vx
-    
-    '''
 
     # Gestion de la pantalla
     pantalla.fill(NEGRO) #Pinta la pantalla de negro antes de cada movimiento
     for bola in bolas:
-        pg.draw.circle(pantalla, bola['color'], (bola['x'], bola['y']), 10)
+        pg.draw.circle(pantalla, bola.color, (bola.x, bola.y), 10)
    
-    # pg.draw.circle(pantalla, ROJO, (x, y), 10) #Dibuja el circulo en el centro de la pantalla como inicio, con su color y tamaño de su radio (10)
-    
-    # pg.draw.circle(pantalla, VERDE, (x2, y2), 10)
 
     pg.display.flip() #Renderiza la pantalla y muestra los cambios producidos por los eventos
 
