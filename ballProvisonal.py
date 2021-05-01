711789f8d3f4312ecbc6c4e078d938d79523d890
Marcos Alonso <marcosalonso@MacBook-Pro-de-Marcos.local>
2021-05-01 09:25:02 +0200

    Primer paso para que la bbola se haga dueña de si misma, incluyendo en la clase Bola su comportamiento, rebotar

--- ballProvisonal.py
+++ ballProvisonal.py
@@ -1,19 +1,8 @@
 import pygame as pg
 import sys
-from random import randint
-
-def rebotaX(x): #Estas funciones sustituye a los if de las lineas 57 y 61, establece un cambio de direccion de la bola si toca con el borde. Se le cambia el signo (return -1), lo que es igual a que adquiere la misma velocidad en sentido contrario
-    if x <= 0 or x >= ANCHO: 
-        return -1
-    
-    return 1
+from random import randint, choice
 
 
-def rebotaY(y):
-    if y <= 0 or y >= ALTO: 
-        return -1
-    
-    return 1
 
 
 ROJO = (255, 0, 0)
@@ -35,31 +24,30 @@ class Bola():
         self.vx = vx
         self.vy = vy
         self.color = color
-        
+
+    def rebota(self):
+        if self.y <= 0 or self.y >= ALTO: 
+            self vy = -self.vy 
+ 
+        if self.x <= 0 or self.x >= ANCHO: 
+             self vx = -self.vx
+    
+   
 
 bolas = []
 for _ in range(10):
     bola = Bola(randint(0, ANCHO), #Sustuyo el diciionario bola por la instancia bola, que llama a la clase Bola
                 randint(0, ALTO), 
-                randint(5, 10), 
-                randint(5, 10),
+                randint(5, 10) * choice([-1, 1]), #choice es un metodo de la clase random para elegir valores al azar
+                randint(5, 10) * choice([-1, 1]),
                 (randint(0, 255), randint(0, 255), randint(0, 255)))
-
-    '''
-    bola = {'x': randint(0, ANCHO), 
-            'y': randint(0, ALTO), 
-            'vx': randint(5, 10), 
-            'vy': randint(5, 10),
-            'color': (randint(0, 255), randint(0, 255), randint(0, 255))
-    }
-    '''
 
     bolas.append(bola)
 
 
 game_over = False
 while not game_over:
-    reloj.tick(60)
+    reloj.tick(30)
     # Gestion de eventos
     for evento in pg.event.get(): #Bucle con instruccion tipo de pygame para la gestión de eventos 
         if evento.type == pg.QUIT:
@@ -71,18 +59,7 @@ while not game_over:
         bola.x += bola.vx
         bola.y += bola.vy
 
-        bola.vy *= rebotaY(bola.y)
-        bola.vx *= rebotaX(bola.x)
-        
-        '''
-        bola['x'] += bola.vx
-        bola.y += bola.vy
-
-        bola.vy *= rebotaY(bola.y)
-        bola.vx *= rebotaX(bola['x'])
-       
-        '''
-
+        bola.rebota()
 
     # Gestion de la pantalla
     pantalla.fill(NEGRO) #Pinta la pantalla de negro antes de cada movimiento
