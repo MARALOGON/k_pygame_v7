76290a6205f1520ad1545668754068a5b873a209
Marcos Alonso <marcosalonso@MacBook-Pro-de-Marcos.local>
2021-05-07 20:39:19 +0200

    Marcador a sprite

--- ball_2.py
+++ ball_2.py
@@ -10,22 +10,24 @@ ALTO = 600
 FPS = 60
 BLANCO = (255, 255, 255)
 
-class Marcador():
+class Marcador(pg.sprite.Sprite):
     def __init__(self, x, y, fontsize=25, color=BLANCO):
+        super().__init__()
         self.fuente = pg.font.SysFont('PTMono.ttc', 30)
-        self.x = x
-        self.y = y
+        self.text = "0"
         self.color = color
+        self.image = self.fuente.render(str(self.text), True, self.color)
+        self.rect = self.image.get_rect(topleft=(x,y))
+        
     
-    def dibuja(self, text, lienzo):
-        image = self.fuente.render(str(text), True, self.color)
-        lienzo.blit(image, (self.x, self.y))
+    def update(self):
+        self.image = self.fuente.render(str(self.text), True, self.color)
 
 
 class Bola(pg.sprite.Sprite):
     def __init__(self, x, y):
-       # pg.sprite.Sprite.__init__(self) Esta forma de llamar a la superclase Bola es lo mismo que la forma de llamarla de la siguiente linea
-       super().__init__()
+       # pg.sprite.Sprite.__init__(self) Esta forma de llamar a la superclase o clase Padre Sprite es lo mismo que la forma de llamarla de la siguiente linea. Lo que hace es incializar esa clase por si tiene algun atributo que haya que inicializar y no lo sabemos
+       super().__init__() #Inicializa la clase padre Sprite, igual que la linea de arriba
        self.image = pg.image.load('./images/ball1.png').convert_alpha()
        self.rect = self.image.get_rect(center=(x,y)) #Este get_rect devuelve un rectangulo del mismo tamaño (en este caso 30x30px) en el que está pintada la imagen que hemos cargado
 
@@ -47,31 +49,36 @@ class Game():
     def __init__(self):
         self.pantalla = pg.display.set_mode((ANCHO, ALTO))
         self.botes = 0
-        self.cuentaGolpes = Marcador(10,10)
+        self.cuentaSegundos = Marcador(10,10)
 
-        self.ballGroup = pg.sprite.Group()
+        self.todoGrupo = pg.sprite.Group()
         for i in range(random.randint(1, 25)):
             bola = Bola(random.randint(0, ANCHO), random.randint(0, ALTO))
-            self.ballGroup.add(bola)
-
-
-        self.bola = Bola(ANCHO//2, ALTO//2)
+            self.todoGrupo.add(bola)
+        self.todoGrupo.add(self.cuentaSegundos)
 
     def bucle_principal(self):
         game_over = False
         reloj = pg.time.Clock()
+        contador_milisegundos = 0
+        segundero = 0
         while not game_over:
-            reloj.tick(FPS)
+            dt = reloj.tick(FPS)
+            contador_milisegundos += dt
+
+            if contador_milisegundos >= 1000:
+                segundero += 1
+                contador_milisegundos = 0
 
             for evento in pg.event.get():
                 if evento.type == pg.QUIT:
                     game_over = True
             
-            self.ballGroup.update()
+            self.cuentaSegundos.text = segundero
+            self.todoGrupo.update()
         
             self.pantalla.fill((0, 0, 0))
-            self.cuentaGolpes.dibuja('Hola', self.pantalla)
-            self.ballGroup.draw(self.pantalla)
+            self.todoGrupo.draw(self.pantalla)
             
 
             pg.display.flip()
